5e66880f40dc4e9fb02b7470f2387a3545df869d
choiyounji <100260416+choiyounji@users.noreply.github.com>
2023-04-27 00:27:59 +0900

    Update symtable.c

--- symtable.c
+++ symtable.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "tn.h"
-//#include "glob.h"
 
-extern int linenum;
+extern int linenum; //라인 줄 수
 extern int yyleng;
 extern char* yytext;
 extern reporterror(enum tokentype tn);
 
-#define STsize 1000 //size of string table
-#define HTsize 100 //size of hash table
+#define STsize 1000 //string table 사이즈
+#define HTsize 100 //hash table 사이즈
 #define FALSE 0
 #define TRUE 1
-
 
 typedef struct HTentry* HTpointer;
 typedef struct HTentry {
@@ -24,56 +22,23 @@ typedef struct HTentry {
 HTpointer HT[HTsize];
 char ST[STsize];
 
-int nextid = 0; //the current identifier
-int nextfree = 0; //the next available index of ST
-int hashcode; //hash code of identifier
-int sameid; //first index of identifier
-int found; //for the previous occurrence of an identifier
-int input;
+int nextid = 0; //현재 identifier
+int nextfree = 0; //ST에서 가능한 다음 index 번호the next available index of ST
+int hashcode; //identifier의 hash code
+int sameid; //identifier의 첫번째 index
+int found; //identifier가 ST에 이미 존재할 경우
 
-/*PrintHStable - Prints the hash table.
-write out the hashcode and the list of identifiers
-associated with each hashcode,
-but only for non-emty lists.
-Print out the number of characters used up in ST*/
-void PrintHStable() {
-	int i, j;
-	HTpointer here;
-	printf("\n\n\n\n\n [[ HASH TABLE ]] \n\n");
-	for (i = 0; i < HTsize; i++) {
-		if (HT[i] != NULL) {
-			printf(" Hash Code %3d : ", i);
-			for (here = HT[i]; here != NULL; here = here->next) {
-				j = here->index;
-				while (ST[j] != '\0' && j < STsize) {
-					printf("%c", ST[j++]);
-				}
-				printf(" ");
-			}
-			printf("\n");
-		}
-	}
-	printf("\n\n\n < %5d characters are used in the string table >\n", nextfree);
-}
-
-
-/* ComputeHS - Compute the hash code of identifier by summing the ordinal values
-of its characters and then taking the sum modulo the size of
-HT.*/
+//identifier의 hash code들을 모두 더하여 modulo연산을 수행
 void ComputeHS(int nid, int nfree) {
 	int code, i;
 	code = 0;
 	for (i = nid; i < nfree - 1; i++) {
-		code += (int)ST[i];
+		code += (int)ST[i];   //hash code더하기
 	}
-	hashcode = code % HTsize;
+	hashcode = code % HTsize;   //modulo
 }
 
-/* LookupHS - For each identifier, Look it up in the hashtable for previous occurrence
-of the identifier. If find a match, set the found flag as
-true.
-Otherwise false.
-If find a match, save the starting index of ST in sameid.*/
+//identifier가 이전에 삽입된 적 있는지 살펴봄 이미 존재한다면 ST에 이미 존재하는 시작 index를 불러옴
 void LookupHS(int nid, int hscode) {
 	HTpointer here;
 	int i, j;
@@ -98,12 +63,9 @@ void LookupHS(int nid, int hscode) {
 		}
 	}
 }
-/* ADDHT - Add a new identifier to the hash table.
-If list head ht[hashcode] is null, simply add a list element with
-starting index of the identifier in ST.
-IF list head is not a null, it adds a new identifier to the head of
-the chain*/
 
+//hash table에 새로운 identifier를 추가
+//null이었으면 바로 추가하고, 이미 존재한다면 linked list로 연결
 void ADDHT(int hscode) {
 	HTpointer ptr;
 	ptr = (HTpointer)malloc(sizeof(ptr));
@@ -112,45 +74,34 @@ void ADDHT(int hscode) {
 	HT[hscode] = ptr;
 }
 
-/* MAIN */
+//메인 함수
 int SymbolTable()
 {
-
 	nextid = nextfree;
 
 	// yytext 한글자씩 ST에 넣음
 	for (int i = 0; i < yyleng; i++) {
 		ST[nextfree++] = yytext[i];
 	}
+	
 	// identifier를 다 넣은 후
 	ST[nextfree++] = '\0';
 
-	// HS계산, 각 identifer에 대해, hastable에 이미 있는지 확인
+	// 각 identifer에 대해 hastable에 이미 있는지를 확인
 	ComputeHS(nextid, nextfree);
 	LookupHS(nextid, hashcode);
-
-
-	if (!found) { // 아직 같은 identifier를 ST에 넣은 적 없을 때
+ 	
+	//처음 등장한 identifier일 경우
+	if (!found) { 
 		// line number, Token-type, ST-index(identifier인 경우), token 순으로 출력
 		printf("%-20d %-30s %-20d %-30s\n", linenum, "Identifier", nextid, yytext);
 		ADDHT(hashcode); // HT 삽입
 	}
-	else { // 이미 같은 identifier가 있을 때
-
+	//identifier가 이미 존재하는 경우
+	else { 
 		// line number, Token-type, ST-index(identifier인 경우), token 순으로 출력
 		printf("%-20d %-30s %-20d %-30s\n", linenum, "Identifier", sameid, yytext);
 		nextfree = nextid; //이번에 ST에 읽은 identifier를 다음에 덮어씀
 	}
 
-	//printf("\n1976393 최윤지 \n  2076278 윤지윤 \n 2076285 이강희");
-	//fclose(fp);
-
-	/*ST출력
-	printf("===ST===\n");
-	for (int i = 0; i < nextfree; i++) {
-	if (ST[i] == '\0') printf("\n");
-	else printf("%c", ST[i]);
-	}
-	printf("\n\n\n");
-	*/
 }
