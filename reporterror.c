88b27160d2e7550d19065c57966c8297113e4c94
choiyounji <100260416+choiyounji@users.noreply.github.com>
2023-04-27 00:16:47 +0900

    Update reporterror.c

--- reporterror.c
+++ reporterror.c
@@ -2,29 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include "tn.h"
-//#include "glob.h"
 extern int linenum; //linenumber->cline
 extern char* yytext;
 extern int cErrors; //에러 개수
 
-/* reporterror
-* token type별로 에러를 나누고 해당 에러문 출력
- 1)ILLSP : 유효하지 않은 문자가 섞인 경우
- 2)ILLID : 숫자로 시작하는 경우
- 3)TOOLONG : 10자 이상인 경우
-*/
+//token type별로 에러를 나누고 해당 에러문 출력
 void reporterror(enum tokentype tn) {
 	switch (tn) {
+	//ILLSP : 유효하지 않은 문자가 섞인 경우
 	case ILLSP:
 		printf("%-20d %-30s %-20s %s is illegal IDENT\n", linenum, "***Error***", " ", yytext);
 		cErrors++;
 		break;
-
+	//ILLID : 숫자로 시작하는 경우
 	case ILLID:
 		printf("%-20d %-30s %-20s %-1s %-30s\n", linenum, "***Error***", " ", yytext, " starts with digit");
 		cErrors++;
 		break;
-
+	//TOOLONG : 10자 이상인 경우
 	case TOOLONG:
 		printf("%-20d %-30s %-20s %-30s %-30s\n", linenum, "***Error***", " ", yytext, "too long IDENT");
 		cErrors++;
